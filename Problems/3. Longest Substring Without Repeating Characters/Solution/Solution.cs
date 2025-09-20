d6e680f14cea8590c709216194f6f1ec966555bd
tokr qui <tokrqui@gmail.com>
2025-09-20 13:14:38 +0500

    3. Longest Substring Without Repeating Characters

---
+++ Problems/3. Longest Substring Without Repeating Characters/Solution/Solution.cs
@@ -0,0 +1,35 @@
+﻿namespace Solution;
+
+public class SolutionComponent {
+    public int LengthOfLongestSubstring(string s)
+    {
+        var chars = s.ToCharArray();
+        var hs = new HashSet<char>();
+
+        for (int w = chars.Length; w > 0; w--)
+        {
+            for (int start = 0; start <= chars.Length - w; start++)
+            {
+                bool hasCollision = false;
+                hs.Clear();
+
+                for (int i = start; i < start + w; i++)
+                {
+                    var currentChar = chars[i];
+                    if (hs.Contains(currentChar))
+                    {
+                        hasCollision = true;
+                        break;
+                    }
+                    hs.Add(currentChar);
+                }
+                if (!hasCollision)
+                {
+                    return hs.Count;
+                }
+            }
+        }
+
+        return 0;
+    }
+}
---
+++ Problems/3. Longest Substring Without Repeating Characters/Tests/TestCases.cs
@@ -0,0 +1,27 @@
+﻿
+namespace Tests;
+
+using Solution;
+using NUnit.Framework;
+
+public class Tests
+{
+    public SolutionComponent SolutionComponentInstance;
+    [SetUp]
+    public void Setup()
+    {
+        SolutionComponentInstance = new SolutionComponent();
+    }
+
+    [Test]
+    [TestCase("abcabcbb", 3)]
+    [TestCase("bbbbb", 1)]
+    [TestCase("pwwkew", 3)]
+    [TestCase("", 0)]
+    [TestCase("au", 2)]
+    public void LengthOfLongestSubstring_Test(string input, int expected)
+    {
+        int result = SolutionComponentInstance.LengthOfLongestSubstring(input);
+        Assert.That(result, Is.EqualTo(expected));
+    }
+}
